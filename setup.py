9e43c0fc57cb586882ea40bf2c3304b236a7fe22
shangguan <shangguan@pku.edu.cn>
2017-02-02 21:36:59 +0800

    setup info

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ setup(
   description = 'Linear regression with MCMC',
   author = 'Jinyi Shangguan',
   author_email = 'shangguan@pku.edu.cn',
+  license="MIT",
   url = 'https://github.com/darkbear9494/LinFit',
   download_url = 'https://github.com/darkbear9494/LinFit/tarball/1.0',
   keywords = ['fitting', 'MCMC', 'linear regression'],
+  install_requires=["numpy", "matplotlib", "emcee"],
   classifiers = [],
 )
