0f13a5845c5dcaaa7a785c46f526f0ec6ced3715
shangguan <shangguan@pku.edu.cn>
2017-02-02 21:00:40 +0800

    add the setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'linfit',
+  packages = ['linfit'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Linear regression with MCMC',
+  author = 'Jinyi Shangguan',
+  author_email = 'shangguan@pku.edu.cn',
+  url = 'https://github.com/darkbear9494/LinFit',
+  download_url = 'https://github.com/darkbear9494/LinFit/tarball/1.0',
+  keywords = ['fitting', 'MCMC', 'linear regression'],
+  classifiers = [],
+)
